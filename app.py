e33c4e9b50947c1c113d88d49df3a06822c060b4
Hoang Quang <hoangkun27@gmail.com>
2025-01-06 04:25:30 +0700

    Remove all files

--- app.py
+++
@@ -1,7 +0,0 @@
-from server import create_app
-from flask_sqlalchemy import SQLAlchemy
-
-app = create_app()
-#migrate = Migrate(app, db)
-if __name__ == '__main__':
-    app.run(debug=True)
--- client/src/Components/index.js
+++
@@ -1,5 +0,0 @@
-import RightSection from "./RightSection/RightSection";
-import LeftSection from "./LeftSection/LeftSection";
-import Footer from "./Footer/Footer";
-
-export { RightSection, LeftSection, Footer };
